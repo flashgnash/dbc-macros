3c0d717cb86b302c6437888a7b6e1a9b8e2572cf
flashgnash <flashgnash@proton.me>
2024-07-16 10:38:01 +0100

    Reapply "Initial testing"
    This reverts commit ed1fd10678d4d00b602a6fc01ca2aafeb92e7063.

---
+++ flake.nix
@@ -0,0 +1,50 @@
+# file: flake.nix
+{
+  description = "Macros for dragon block C";
+
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = nixpkgs.legacyPackages.${system};
+      # create a custom "mkPoetryApplication" API function that under the hood uses
+      # the packages and versions (python3, poetry etc.) from our pinned nixpkgs above:
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      myPythonApp = mkPoetryApplication { projectDir = ./.; };
+    in
+    {
+      apps.${system} = {
+        multiwield = {
+          type = "app";
+          # replace <script> with the name in the [tool.poetry.scripts] section of your pyproject.toml
+          program = "${myPythonApp}/bin/multiwield";
+        };      
+      
+        senzu = {
+          type = "app";
+          # replace <script> with the name in the [tool.poetry.scripts] section of your pyproject.toml
+          program = "${myPythonApp}/bin/senzu";
+        };
+      };
+           
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodePackages.prettier
+          poetry
+
+          linuxHeaders
+          pkgs.python3Packages.pynput
+        ];
+      };
+      
+    };
+}
